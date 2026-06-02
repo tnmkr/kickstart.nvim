ca1121a715173ebbea3c3833a95cf3eff1c9ee0f
Tanmay Kumar <tnmdotkr@gmail.com>
2026-06-02 09:11:05 +0530

    Added folding using tresitter

--- init.lua
+++ init.lua
@@ -864,8 +864,9 @@ do
 
 		-- Enable treesitter based folds
 		-- For more info on folds see `:help folds`
-		-- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-		-- vim.wo.foldmethod = 'expr'
+		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.wo.foldmethod = "expr"
+		vim.o.foldlevelstart = 0
 
 		-- Check if treesitter indentation is available for this language, and if so enable it
 		-- in case there is no indent query, the indentexpr will fallback to the vim's built in one
